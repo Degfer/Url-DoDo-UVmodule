fbee3bf726dbebdb9ba4841d0a14e088006d70ec
Danil <jetnt123456@gmail.com>
2024-05-09 03:44:00 +0300

    Update load_webview.py

--- load_webview.py
+++ load_webview.py
@@ -1,3 +1,10 @@
+from jnius import autoclass
+from android.runnable import run_on_ui_thread
+
+WebView = autoclass('android.webkit.WebView')
+WebViewClient = autoclass('android.webkit.WebViewClient')
+activity = autoclass('org.kivy.android.PythonActivity').mActivity
+
 def create_webview(thelistitem):
 	webview = WebView(activity)
 	webview.getSettings().setJavaScriptEnabled(True)
